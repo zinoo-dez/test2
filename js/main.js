2f219f1882e6cf86f69efcc02ee08e41d79667b3
zinoo-jsx <zinoo.dev@gmail.com>
2023-03-19 14:33:09 +0630

    fixed in indexjs

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ let nav_links = document.querySelector(".nav-links");
 bars.addEventListener("click", function () {
   //   alert("hello");
   nav_links.classList.toggle("show");
-  console.log(this.src);
-  if (this.src === "http://127.0.0.1:5500/img/bar.svg") {
-    this.src = "http://127.0.0.1:5500/img/cross.svg";
+  console.log(window.location.origin);
+  let url = window.location.origin;
+  if (this.src === `${url}/img/bar.svg`) {
+    this.src = `${url}/img/cross.svg`;
+    // console.log(this.src);
   } else {
-    this.src = "http://127.0.0.1:5500/img/bar.svg";
+    this.src = `${url}/img/bar.svg`;
+    // console.log(this.src);
   }
 });
