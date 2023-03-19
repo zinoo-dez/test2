5368ef0e74a803301729a81c10b797e4b6049080
zinoo-jsx <zinoo.dev@gmail.com>
2023-03-19 14:24:18 +0630

    fixed in indexjs

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ bars.addEventListener("click", function () {
   //   alert("hello");
   nav_links.classList.toggle("show");
   console.log(this.src);
-  if (this.src === "http://127.0.0.1:5500/day8/blog/img/bar.svg") {
-    this.src = "http://127.0.0.1:5500/day8/blog/img/cross.svg";
+  if (this.src === "http://127.0.0.1:5500/img/bar.svg") {
+    this.src = "http://127.0.0.1:5500/img/cross.svg";
   } else {
-    this.src = "http://127.0.0.1:5500/day8/blog/img/bar.svg";
+    this.src = "http://127.0.0.1:5500/img/bar.svg";
   }
 });
